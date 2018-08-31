54920fc0f6551a2d70de6fc9aae20bda33b5be8d
Somil <somilshah112@gmail.com>
2018-08-31 18:28:17 +0530

    Update breast_cancer_prediction.py

--- breast_cancer_prediction.py
+++ breast_cancer_prediction.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 31 00:01:13 2018
-
-@author: Somil
+@author: Somil Shah
 """
 
 import numpy as np
@@ -14,23 +12,27 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 
+#Scanning the dataset to be used and selecting the columns needed as input and output
 dataset=pd.read_csv("data.csv")
 X=dataset.iloc[:,2:32].values
 Y=dataset.iloc[:,[1,]].values
 
+#Label encoding the categorical outputs
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_Y_1 = LabelEncoder()
 Y[:,0] = labelencoder_Y_1.fit_transform(Y[:,0])
 Y=Y.astype(float)
 
+#Scaling all the inputs to values between -1 and 1 to avoid biasing of input values
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X=sc.fit_transform(X)
 
+#Splitting data into training and test sets
 from sklearn.model_selection import train_test_split
-
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=42,shuffle=True)
 
+#Creating the neural network with 30 as input units and 2 hidden layers with 16 units
 classifier = Sequential()
 
 classifier.add(Dense(units = 16, kernel_initializer = 'uniform', activation = 'relu', input_dim = 30))
@@ -43,6 +45,7 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 
 classifier.fit(X_train, Y_train, batch_size = 25, epochs = 50,validation_data=(X_test,Y_test))
 
+#Predicting the data on untrained set
 y_pred=classifier.predict(X_test)
 y_pred=(y_pred>0.5)
 
